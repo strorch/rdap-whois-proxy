c437d7ea7efe6277859c7f8282169a4f5e5daa22
strorch <smy980807@ukr.net>
2019-09-02 21:29:30 +0300

    creating provider

--- src/Provider/WhoisDomainProvider.php
+++ src/Provider/WhoisDomainProvider.php
@@ -7,7 +7,12 @@ use hiqdev\rdap\core\Domain\Entity\Domain;
 use hiqdev\rdap\core\Domain\Entity\Nameserver;
 use hiqdev\rdap\core\Domain\ValueObject\DomainName;
 use hiqdev\rdap\core\Infrastructure\Provider\DomainProviderInterface;
+use Iodev\Whois\Modules\Tld\DomainInfo;
 use Iodev\Whois\Whois;
+use yii\db\Exception;
+
+//use phpWhois\Whois as Whois;
+//use phpWhois\Whois;
 
 class WhoisDomainProvider implements DomainProviderInterface
 {
@@ -23,21 +28,41 @@ class WhoisDomainProvider implements DomainProviderInterface
 
     public function get(DomainName $domainName): Domain
     {
+        if (!$this->whois->isDomainAvailable("google.com")) {
+            throw new Exception('domain is not available');
+        }
+        /** @var DomainInfo $domainInfo */
         $domainInfo = $this->whois->loadDomainInfo($domainName->toLDH());
 
-        $domain = new Domain($domainInfo->getDomainName());
+        $domain = new Domain(DomainName::of($domainInfo->getDomainName()));
         foreach ($domainInfo->getNameServers() as $nameServer) {
             $domain->addNameserver(new Nameserver(DomainName::of($nameServer)));
         }
-        foreach ($domainInfo->getStates() as $state) {
-            $domain->addStatus(Status::byName($state));
+        $statuses = $domainInfo->getStates();
+        foreach ($statuses as $state) {
+            $domain->addStatus(Status::byName(strtoupper($state)));
         }
         // todo:
-        $domainInfo->getCreationDate();
-        $domainInfo->getExpirationDate();
-        $domainInfo->getRegistrar();
-        $domainInfo->get(); // try to obtain contacts
+        $creationDate = $domainInfo->getCreationDate();
+        $expirationDate = $domainInfo->getExpirationDate();
+        $registrarInfo = $domainInfo->getRegistrar();
+        $owner = $domainInfo->getOwner();
+        $whoisServer = $domainInfo->getWhoisServer();
+        $domainInfo->getDomainNameUnicode();
+
+//        $domainInfo->getVal();
+
+//        $domainInfo->get(); // try to obtain contacts
 
         return $domain;
     }
+
+//    public function get(DomainName $domainName): Domain
+//    {
+//        $whois = new NewWhois();
+//        $res = $whois->loo;
+//        $domain = new Domain(DomainName::of('kek'));
+//
+//        return $domain;
+//    }
 }
